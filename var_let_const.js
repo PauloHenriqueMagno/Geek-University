ef5ae383bec09eabf961371bc39536a7f58d75df
PauloHenriqueMagno <79842305+PauloHenriqueMagno@users.noreply.github.com>
2021-04-29 10:55:52 -0300

    realizando o curso

--- var_let_const.js
+++ var_let_const.js
@@ -16,8 +16,51 @@ let nome = 'Geek';
 nome = 'University';
 console.log(nome);
 
-for(var i = 0; i < 5; i++){
-    var valor = i * 3;
-    console.log(valor);
+for(let i = 0; i < 5; i++){
+    let valor = i * 3;
+    console.log('valor: ' + valor);
+    console.log('valor de i: ' + i);
 }
 
+//console.log(valor); //reference error
+//console.log(i); //reference error
+
+//let numero = 88;
+//console.log(numero);
+
+//let numero = 32; // SyntaxError
+//console.log(numero);
+
+// const
+const TAXA = 1.44;
+console.log(TAXA);
+
+let res= 45 * TAXA;
+console.log(res);
+
+//TAXA = 5; //TypeError
+//console.log(TAXA);
+
+//Constante vs Mutação
+const curso = {nome: "Programação em JavaScript"};
+console.log(curso.nome);
+
+// Não posso alterar o valor em dados contidos na constante
+//curso = 43; //Error
+
+//Podemos realizar mutação em dados contidos na constante
+curso.nome = "Programação em Python"; // Mutação
+//console.log(curso);
+
+/*
+Dicas de como declarar variáveis em JavaScript
+
+- A variável poderá ser alterada? (vai variar?) se sim, use let
+- A variável será constante? (não vai variar?) se sim, use const
+*/
+
+const valor = 5;
+
+for(let i = 0; i < valor; i++){
+    console.log(valor - i);
+}
